11fad0c12f10bab7eb4a77218236412255526792
ChilyGarcia <jugada160324@gmail.com>
2026-02-25 21:18:51 -0500

    feat: IMplemented error

--- main.ts
+++ main.ts
@@ -1,4 +1,4 @@
-import { Application, Router, Server, oakCors } from "./deps.ts";
+import { Application } from "./deps.ts";
 import { index_router } from "./routes/router.ts";
 import { MONGODB_URI } from "./utils/env.ts";
 import deserialize_user from "./middlewares/deserializeUser.ts";
@@ -6,24 +6,14 @@ import deserialize_target_user from "./middlewares/deserializeTargetUser.ts";
 import deserialize_body from "./middlewares/deserializeBody.ts";
 import { io_consoles } from "./sockets/console.socket.ts";
 import { init_settings } from "./utils/initSettings.utils.ts";
-import { Cookies } from "https://deno.land/x/oak@v14.0.0/mod.ts";
 
 const app = new Application();
 
 // Socket.io for console handling.
 io_consoles;
-init_settings()
+init_settings();
 
-// CORS: permitir cualquier origen (refleja el Origin del request)
-app.use(
-    oakCors({
-        origin: true,
-        methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
-        allowedHeaders: ["Content-Type", "Authorization", "Origin", "X-Auth-Token"],
-        credentials: true,
-    })
-);
-
+// CORS lo maneja solo Nginx (evita cabeceras duplicadas: backend + Nginx)
 app.use(deserialize_body);
 app.use(deserialize_user);
 app.use(deserialize_target_user);
